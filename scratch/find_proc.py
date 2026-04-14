210cc24ea94fce6dba996a9fb05af8a999e360ea
MUKUL <mukulprasad958@gmail.com>
2026-04-14 17:52:38 +0530

    v

--- scratch/find_proc.py
+++
@@ -1,5 +0,0 @@
-import psutil
-
-for p in psutil.process_iter(['pid', 'name', 'cmdline', 'cwd']):
-    if p.info['cmdline'] and 'buildmind' in ' '.join(p.info['cmdline']):
-        print(f"PID: {p.info['pid']}, CWD: {p.info['cwd']}, CMD: {' '.join(p.info['cmdline'])}")
--- scratch/kill_proc.py
+++
@@ -1,9 +0,0 @@
-import psutil
-
-for p in psutil.process_iter(['pid', 'name', 'cmdline']):
-    if p.info['cmdline'] and 'buildmind' in ' '.join(p.info['cmdline']):
-        print(f"Killing PID: {p.info['pid']}")
-        try:
-            p.kill()
-        except Exception as e:
-            print(f"Failed: {e}")
